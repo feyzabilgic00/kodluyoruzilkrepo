f37336cd17c66eeffec4e5103f90e07e4dcd01a8
feyzabilgic00 <50487013+feyzabilgic00@users.noreply.github.com>
2021-04-30 11:39:28 +0300

    Odev1

---
+++ deneme.js
@@ -0,0 +1,15 @@
+console.log("merhaba dünya");
+
+console.log(50%7);
+
+var result="I love React".split("").reverse().join("");
+
+console.log(result);
+
+var result2=5==="5";
+
+console.log(result2);
+
+var colors=["red","green","blue"];
+
+console.log(colors);
